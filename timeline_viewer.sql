24bec55f4b2ef42c92d6332d25d4f72ec6266ed4
sloanfardele <sloan.fardele@outlook.fr>
2016-09-02 16:31:40 +0200

    ajout du fichier sql pour la création des tables

---
+++ timeline_viewer.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Ven 02 Septembre 2016 à 16:30
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `timeline_viewer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tv_events`
+--
+
+CREATE TABLE IF NOT EXISTS `tv_events` (
+  `id_event` int(11) NOT NULL AUTO_INCREMENT,
+  `id_timeline` int(11) NOT NULL,
+  `date_event` date NOT NULL,
+  `name_event` varchar(250) NOT NULL,
+  `desc_event` varchar(250) NOT NULL,
+  PRIMARY KEY (`id_event`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `tv_events`
+--
+
+INSERT INTO `tv_events` (`id_event`, `id_timeline`, `date_event`, `name_event`, `desc_event`) VALUES
+(1, 1, '2016-06-14', 'Un truc de ouf', 'Il s''est passé un truc de malade wllh'),
+(2, 1, '2016-08-10', '4 ans', '4 ans avec la plus belle ');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tv_timelines`
+--
+
+CREATE TABLE IF NOT EXISTS `tv_timelines` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(250) NOT NULL,
+  `date_de_creation` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Contenu de la table `tv_timelines`
+--
+
+INSERT INTO `tv_timelines` (`id`, `nom`, `date_de_creation`) VALUES
+(1, 'test', '2016-09-01'),
+(8, 'Anne', '2016-09-01');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
